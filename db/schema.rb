8d14bff49f7a2522a8cc385e6c1b07fd700dc6aa
吉野輝 <shoynoi.jp@gmail.com>
2019-06-02 07:51:15 +0900

    tasksのnameにNOT NULL制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_01_041232) do
+ActiveRecord::Schema.define(version: 2019_06_01_224654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
