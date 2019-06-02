4a87f0a4bedc3ff8a457ef1c3d627a14a1461f8c
吉野輝 <shoynoi.jp@gmail.com>
2019-06-02 11:12:38 +0900

    スコープの作成

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,7 @@ class Task < ApplicationRecord
   validate :validate_name_not_including_comma
   belongs_to :user
 
+  scope :recent, -> { order(created_at: :desc) }
   private
 
   def validate_name_not_including_comma
