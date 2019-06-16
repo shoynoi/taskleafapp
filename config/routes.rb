f7a477dd2a46185d6b3f3c531a4ba527ac94d413
吉野輝 <shoynoi.jp@gmail.com>
2019-06-16 16:18:10 +0900

    タスクの新規登録前に確認画面を表示

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
-  resources :tasks
+  resources :tasks do
+    post :confirm, action: :confirm_new, on: :new
+  end
 end
