6eeea07be6aacc347b00b672f29ed9268771e9c0
吉野輝 <shoynoi.jp@gmail.com>
2019-06-16 17:51:32 +0900

    タスクに画像添付機能を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -49,7 +49,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name, :description)
+    params.require(:task).permit(:name, :description, :image)
   end
 
   def set_task
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
+  has_one_attached :image
   validates :name, presence: true, length: { maximum: 30 }
   validate :validate_name_not_including_comma
   belongs_to :user
