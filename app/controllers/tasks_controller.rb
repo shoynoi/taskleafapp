67282555363af9b96ee1f3f2bb80d0694723dd0c
吉野輝 <shoynoi.jp@gmail.com>
2019-06-16 16:46:49 +0900

    タスクのソート機能を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @q = current_user.tasks.ransack(params[:q])
-    @tasks = @q.result(distinct: true).recent
+    @tasks = @q.result(distinct: true)
   end
 
   def show
