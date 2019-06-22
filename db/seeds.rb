0d0d15c31df5d1d4015307fc049defbd18daade8
吉野輝 <shoynoi.jp@gmail.com>
2019-06-22 13:44:14 +0900

    seedデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.find_or_create_by!(email: "admin@example.com") do |user|
+  user.name = "admin"
+  user.admin = true
+  user.password = "password"
+  user.password_confirmation = "password"
+end
