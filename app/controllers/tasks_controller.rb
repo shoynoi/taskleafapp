325aed3c49715966c5c1a5591ff376462880f41b
吉野輝 <shoynoi.jp@gmail.com>
2019-06-26 08:26:42 +0900

    タスクの更新時に不正な値が入力された時再入力を促すように修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -42,8 +42,11 @@ class TasksController < ApplicationController
   end
 
   def update
-    @task.update!(task_params)
-    redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
+    if @task.update(task_params)
+      redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
+    else
+      render :edit
+    end
   end
 
   def destroy
