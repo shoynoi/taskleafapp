71f56744de48ecef017a2e566e7d9a71a8941520
吉野輝 <shoynoi.jp@gmail.com>
2019-07-02 19:47:08 +0900

    ユーザーに紐付いたタスクを削除するように修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
 end
